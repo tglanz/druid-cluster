a46745ff9faf10707b635b1b982dc951d4aef6cf
Tal Glanzman <talglanzman@gmail.com>
2022-06-28 22:53:10 +0300

    ingress

--- infra/scripts/delete-from-k8s.sh
+++ infra/scripts/delete-from-k8s.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 
 namespace=druid
 
-for name in metadata zookeeper druid-master druid-query; do
+for name in metadata zookeeper druid-master druid-query ingress; do
     kubectl --namespace $namespace delete -f ../$name/$name.yaml
 done
--- infra/scripts/deploy-to-k8s.sh
+++ infra/scripts/deploy-to-k8s.sh
@@ -8,6 +8,6 @@ for name in node master query; do
     ../druid-$name/build-image.sh
 done
 
-for name in metadata zookeeper druid-master druid-query; do
+for name in metadata zookeeper druid-master druid-query ingress; do
     kubectl --namespace $namespace apply -f ../$name/$name.yaml
 done
